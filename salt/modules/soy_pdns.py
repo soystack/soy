32571bf69f5334a48da4118c452f01de75dea181
jamesalbert <james.albert72@gmail.com>
2013-08-21 13:01:37 -0700

    soy_pdns.py class methods to functions

--- salt/modules/soy_pdns.py
+++ salt/modules/soy_pdns.py
@@ -2,51 +2,26 @@
 
 from soy.pdns import Domain, Record
 
-class domain(object):
-	def __init__(self, **kwargs):
-		self.dns = Domain(__salt__, **kwargs)
+def create_domain(self):
+	return Domain(__salt__, **kwargs).create()
 
-	def create(self):
-		return self.dns.create()
+def report_domain(self):
+	return Domain(__salt__, **kwargs).report()
 
-	def report(self):
-		return self.dns.report()
+def update_domain(self):
+	return Domain(__salt__, **kwargs).update()
 
-	def update(self):
-		return self.dns.update()
+def delete_domain(self):
+	return Domain(__salt__, **kwargs).delete()
 
-	def delete(self):
-		return self.dns.delete()
+def create_record(self):
+	return Record(__salt__, **kwargs).create()
 
-class record(object):
-	def __init__(self, **kwargs):
-		self.dns = Record(__salt__, **kwargs)
+def report_record(self):
+	return Record(__salt__, **kwargs).report()
 
-	def create(self):
-		return self.dns.create()
+def update_record(self):
+	return Record(__salt__, **kwargs).update()
 
-	def report(self):
-		return self.dns.report()
-
-	def update(self):
-		return self.dns.update()
-
-	def delete(self):
-		return self.dns.delete()
-
-def start_domain(kwargs):
-	func = kwargs.get('func', None)
-	for attr in dir(domain):
-		if func is attr:
-			return getattr(domain(**kwargs), func)()
-
-	return {'status': 'function %s failed' % func}
-		
-
-def start_record(func, **kwargs):
-	func = kwargs.get('func', None)
-	for attr in dir(record):
-		if func is attr:
-			return getattr(record(**kwargs), func)()
-
-	return {'status': 'function %s failed' % func}
+def delete_record(self):
+	return Record(__salt__, **kwargs).delete()
