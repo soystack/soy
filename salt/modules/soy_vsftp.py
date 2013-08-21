c2257a6ee46fffcb82cb244790438865da9909e8
jamesalbert <james.albert72@gmail.com>
2013-08-20 18:20:24 -0700

    soy_vsftp.py class to functions

--- salt/modules/soy_vsftp.py
+++ salt/modules/soy_vsftp.py
@@ -2,21 +2,14 @@
 
 from soy.vsftp import User
 
-class user(object):
-	def __init__(self, **kwargs):
-		self.ftp = User(__salt__, **kwargs)
+def create(self):
+	return User(__salt__, **kwargs).create()
 
-	def create(self):
-		return self.ftp.create()
+def report(self):
+	return User(__salt__, **kwargs).report()
 
-	def report(self):
-		return self.ftp.report()
+def update(self):
+	return User(__salt__, **kwargs).update()
 
-	def update(self):
-		return self.ftp.update()
-
-	def delete(self):
-		return self.ftp.delete()
-
-def start_ftp(kwargs):
-	return getattr(user(**kwargs), kwargs['func'])()
+def delete(self):
+	return User(__salt__, **kwargs).delete()
