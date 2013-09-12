9415c3af722151e6a263b44800b7afd9b252fa44
James Albert <james.albert72@gmail.com>
2013-09-12 10:11:48 -0700

    testing callbacks

--- api/run.py
+++ api/run.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
 from eve import Eve
+from salt.router import route
 
 app = Eve()
 app.run()
+
+# something like this
+def nginx_create(request, payload):
+  return route('nginx', 'Host', 'create', request)
+
+app.on_GET_nginx_create += nginx_create
