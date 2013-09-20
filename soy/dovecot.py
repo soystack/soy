a38e022c43b3b85456434a43d303d5a906a72a07
James Albert <james.albert72@gmail.com>
2013-09-20 10:19:47 -0700

    initial dovecot commit

---
+++ soy/dovecot.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import MySQLdb
+from soy import mysql
+
+class Dovecot:
+	def __init__(self, **kwargs):
+		self.mysql = mysql.Setup(__salt__, 'dovecot').mysql
+		for k, v in kwargs.iteritems():
+			setattr(self, k, v)
+			
+        try:
+            self.db = MySQLdb.connect(**self.mysql)
+            self.curs = self.db.cursor()
+        except:
+            return None
+		
+	def forward(self):
+		try:
+			self.curs.execute("""INSERT INTO virtual_aliases
+											 (`id`,
+											  `domain_id`,
+											  `source`,
+											  `destination`)
+								 VALUES		 (NULL,
+								 			  $(domain)s,
+								 			  $(source)s,
+								 			  $(destination)s)""", self)
+			self.db.commit()
+			return {'status': True}
+		except:
+			return {'status': False}
