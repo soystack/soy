d957b8659ebcb2fd797956413458e082ea6329c1
James Albert <james.albert72@gmail.com>
2013-09-25 10:03:28 -0700

    new way of routing methods

--- salt/modules/soy_router.py
+++ salt/modules/soy_router.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
 
-import ast
 from soy import *
-
-def call(p, c, kwargs):
-	a = getattr(p, c)
-	if kwargs:
-		return a(**kwargs)
-	else:
-		return a()
 
 def route(service, module, method, opts):
 	'''
@@ -23,9 +15,9 @@ def route(service, module, method, opts):
 		LocalClient.cmd( minion, 'soy_router.route', ['service_name', 'class_name', 'method_name', kwargs] )
 
 	'''
-	kwargs = ast.literal_eval(opts)
-	service = call( globals()[service], module, kwargs)
-	return call(service, method)
+	lib = 'soy.%s' % service
+	eval 'from %s import %s' % module
+	getattr(globals()[module](**kwargs), method)()
 
 	'''
 	soy.Nginx.Host(**kwargs).create()
