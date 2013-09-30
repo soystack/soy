fdd41af9e37f43a800898bf35c8b55969163a5d6
James Albert <james.albert72@gmail.com>
2013-09-29 20:38:44 -0700

    adaption to new lib format

--- api/main.py
+++ api/main.py
@@ -15,42 +15,37 @@ NGINX
 
 @app.route('/nginx/create/<user>/<host>', methods=['GET'])
 def nginxcreate(user, host):
-    user = 'nginx.localdomain'
-    opts = {'user': user,
-            'host': host}
-    return jsonify(c.cmd(user, 'soy_router.route', ['nginx', 'Host', 'create', opts]))
+    minion = 'nginx.localdomain'
+    return jsonify(c.cmd(minion, 'soy_nginx.create', [user, host]))
 
-@app.route('/nginx/report', methods=['GET'])
-def nginxreport():
-    user = 'nginx.localdomain'
-    return jsonify(c.cmd(user, 'soy_router.route', ['nginx', 'Host', 'report', {}]))
+@app.route('/nginx/report/<user>', methods=['GET'])
+def nginxreport(user):
+    minion = 'nginx.localdomain'
+    return jsonify(c.cmd(minion, 'soy_nginx.report', [user]))
 
-@app.route('/nginx/update/<host>/<updated_host>', methods=['PUT'])
-def nginxupdate(host, updated_host):
-    user = 'nginx.localdomain'
-    opts = {'host': host,
-        'updated_host': updated_host}
-    return jsonify(user, 'soy_router.route', ['nginx','Host', 'update', opts])
+@app.route('/nginx/update/<user>/<host>/<updated_host>', methods=['PUT'])
+def nginxupdate(user, host, updated_host):
+    minion = 'nginx.localdomain'
+    return jsonify(c.cmd(minion, 'soy_nginx.update', [user, host, updated_host]))
 
-@app.route('/nginx/delete/<host>', methods=['GET'], defaults={'user': False})
-@app.route('/nginx/delete/<host>/<user>')
+@app.route('/nginx/delete/<user>/<host>', methods=['GET'])
 def nginxdelete(user, host):
-    user = 'nginx.localdomain'
+    minion = 'nginx.localdomain'
+    return jsonify(c.cmd(user, 'soy_nginx.delete', [user, host]))
+
+@app.route('/nginx/suspend/<user>/<host>', methods=['GET'])
+def nginxsuspend(user, host):
+    minion = 'nginx.localdomain'
     opts = {'user': user,
             'host': host}
-    return jsonify(c.cmd(user, 'soy_router.route', ['nginx', 'Host', 'delete', opts]))
+    return jsonify(c.cmd(minion, 'soy_nginx.suspend', [**opts]))
 
-@app.route('/nginx/suspend/<host>', methods=['GET'])
-def nginxsuspend(host):
-    user = 'nginx.localdomain'
-    opts = {'host': host}
-    return jsonify(c.cmd(user, 'soy_router.route', ['nginx', 'Host', 'suspend', opts]))
-
-@app.route('/nginx/unsuspend/<host>', methods=['GET'])
-def nginxunsuspend(host):
-    user = 'nginx.localdomain'
-    opts = {'host': host}
-    return jsonify(c.cmd(user, 'soy_router.route', ['nginx', 'Host', 'unsuspend', opts]))
+@app.route('/nginx/unsuspend/<user>/<host>', methods=['GET'])
+def nginxunsuspend(user, host):
+    minion = 'nginx.localdomain'
+    opts = {'user': user,
+            'host': host}
+    return jsonify(c.cmd(minion, 'soy_nginx.unsuspend', [**opts]))
 
 '''
 DNS
@@ -68,69 +63,76 @@ def dnsdomain():
 def dnsrecord():
     return render_template('records.html')
 
-@app.route('/dns/create/<name>')
-def dnscreate(name):
-    user = 'powerdns.com'
+@app.route('/dns/create/<name>/<master>/<last_check>/<d_type>/<not_srl>/<account>/<ttl>/<prio>/<d_id>')
+def dnscreate(name, master, last_check, d_type, not_srl, account, ttl, prio, d_id):
+    minion = 'powerdns.com'
     status = {}
-    kwargs = {'name': name}
-    status['domain'] = c.cmd(user, 'soy_router.route', ['pdns', 'Domain', 'create', kwargs])
-    status['record'] = c.cmd(user, 'soy_router.route', ['pdns', 'Record', 'create', kwargs])
+    dopts = {'name': name,
+             'master': master,
+             'last_check': last_check,
+             'type': d_type,
+             'notified_serial': not_srl,
+             'account': account}
+    ropts = {'name': name,
+             'ttl': ttl,
+             'prio': prio,
+             'last_check': last_check,
+             'd_id': d_id}
+    status['domain'] = c.cmd(minion, 'soy_pdns.createdomain', [**dopts])
+    status['record'] = c.cmd(minion, 'soy_pdns.createrecord', [**ropts])
     return jsonify(status)
 
 @app.route('/dns/report/domain')
 def dnsreportdomain():
-    user = 'powerdns.com'
-    kwargs, status = {}, {}
-    report = c.cmd(user, 'soy_router.route', ['pdns', 'Domain', 'report', kwargs])
-    for domain in report[user]:
+    minion = 'powerdns.localhost'
+    status = {}
+    report = c.cmd(minion, 'soy_pdns.reportdomain', [])
+    for domain in report[minion]:
         status[domain['name']] = domain
     return jsonify(status)
 
 @app.route('/dns/update/domain/<e_id>/<name>/<master>/<last_check>/<e_type>/<notified_serial>/<account>')
 def dnsupdatedomain(name, e_id, e_type, account, last_check, notified_serial, master):
-    user = 'powerdns.com'
-    kwargs = {'name':name,
-              'e_id':e_id,
-              'e_type':e_type,
-              'account':account,
-              'last_check':last_check,
-              'notified_serial':notified_serial,
-              'master':master}
-    status = c.cmd(user, 'soy_router.route', ['pdns', 'Domain', 'update', kwargs])
+    minion = 'powerdns.localhost'
+    opts = {'name': name,
+            'id': e_id,
+            'type': e_type,
+            'account': account,
+            'last_check': last_check,
+            'notified_serial': notified_serial,
+            'master': master}
+    status = c.cmd(user, 'soy_pdns.updatedomain', [**opts])
     return jsonify(status)
 
 @app.route('/dns/delete/domain/<e_id>')
 def dnsdeletedomain(e_id):
-    user = 'powerdns.com'
-    kwargs = {'e_id': e_id}
-    status = c.cmd(user, 'soy_router.route', ['pdns', 'Domain', 'delete', kwargs])
+    minion = 'powerdns.localhost'
+    status = c.cmd(minion, 'soy_pdns.deletedomain', [e_id])
     return jsonify(status)
 
 @app.route('/dns/report/record')
 def dnsreportrecord():
-    user = 'powerdns.com'
+    minion = 'powerdns.localhost'
     status = {}
-    kwargs = {}
-    report = c.cmd(user, 'soy_router.route', ['pdns', 'Record', 'report', kwargs])
-    for domain in report[user]:
+    report = c.cmd(minion, 'soy_pdns.reportrecord', [])
+    for domain in report[minion]:
         status[domain['id']] = domain
     return jsonify(status)
 
-@app.route('/dns/update/record/<e_id>/<domain_id>/<name>/<e_type>/<content>/<ttl>/<prio>/<change_date>/<ordername>/<auth>')
-def dnsupdaterecord(e_id, domain_id, name, e_type, content, ttl, prio, change_date, ordername, auth):
-    user = 'powerdns.com'
-    kwargs = {'e_id':      e_id,      'domain_id':   domain_id, 'name': name,
-              'e_type':    e_type,    'content':     content,   'ttl':  ttl,
-              'prio':      prio,      'change_date': change_date,
-              'ordername': ordername, 'auth':        auth}
-    status = c.cmd(user, 'soy_router.route', ['pdns', 'Record', 'update', kwargs])
+@app.route('/dns/update/record/<account>/<e_id>/<domain_id>/<name>/<e_type>/<content>/<ttl>/<prio>/<change_date>/<ordername>/<auth>')
+def dnsupdaterecord(account, e_id, domain_id, name, e_type, content, ttl, prio, change_date, ordername, auth):
+    minion = 'powerdns.localhost'
+    opts = {'e_id':      e_id,      'domain_id':   domain_id, 'name': name,
+            'e_type':    e_type,    'content':     content,   'ttl':  ttl,
+            'prio':      prio,      'change_date': change_date,
+            'ordername': ordername, 'auth':        auth}
+    status = c.cmd(minion, 'soy_pdns.updaterecord', [**opts])
     return jsonify(status)
 
 @app.route('/dns/delete/record/<e_id>')
 def dnsdeleterecord(e_id):
-    user = 'powerdns.com'
-    kwargs = {'e_id':e_id}
-    status = c.cmd(user, 'soy_router.route', ['pdns', 'Record', 'delete', kwargs])
+    minion = 'powerdns.localhost'
+    status = c.cmd(user, 'soy_pdns.deleterecord', [e_id])
     return jsonify(status)
 
 '''
@@ -139,30 +141,23 @@ FTP
 
 @app.route('/ftp/create/<user>/<pswd>')
 def ftpcreate(user, pswd):
-    user = 'vsftp.localdomain'
-    kwargs = {'user': user,
-              'pswd': pswd}
-    return jsonify(c.cmd(user, 'soy_router.route', ['vsftp', 'User', 'create', kwargs]))
+    minion = 'vsftp.localdomain'
+    return jsonify(c.cmd(minion, 'soy_vsftp.create', [user, pswd]))
 
 @app.route('/ftp/report')
 def ftpreport():
-    user = 'vsftp.localdomain'
-    kwargs = {}
-    return jsonify(c.cmd(user, 'soy_router.route', ['vsftp', 'User', 'report', kwargs]))
+    minion = 'vsftp.localdomain'
+    return jsonify(c.cmd(minion, 'soy_vsftp.report', []))
 
 @app.route('/ftp/update/<user>/<newuser>/<newpswd>')
 def ftpupdate(user, newuser, newpswd):
-    user = 'vsftp.localdomain'
-    kwargs = {'user': user,
-              'newuser': newuser,
-              'newpswd': newpswd}
-    return jsonify(c.cmd(user, 'soy_router.route', ['vsftp', 'User', 'update', kwargs]))
+    minion = 'vsftp.localdomain'
+    return jsonify(c.cmd(minion, 'soy_vsftp.update', [user, newuser, newpswd]))
 
 @app.route('/ftp/delete/<user>')
 def ftpdelete(user):
-    user = 'vsftp.localdomain'
-    kwargs = {'user': user}
-    return jsonify(c.cmd(user, 'soy_router.route', ['vsftp', 'User', 'delete', kwargs]))
+    minion = 'vsftp.localdomain'
+    return jsonify(c.cmd(minion, 'soy_vsftp.delete', [user]))
 
 '''
 MAIL
@@ -170,25 +165,24 @@ MAIL
 
 @app.route('/mail/forward/<domain>/<source>/<dest>', methods=['POST'])
 def mailforward(domain, source, dest):
-    user = 'mail.localdomain'
-    kwargs = {'domain': domain,
-              'source': source,
-              'destination': dest}
-    return jsonify(c.cmd(user, 'soy_router.route', ['dovecot', 'Mail', 'forward', kwargs]))
+    minion = 'mail.localdomain'
+    opts = {'domain': domain,
+            'source': source,
+            'destination': dest}
+    return jsonify(c.cmd(minion, 'soy_dovecot.forward', [**opts]))
 
 @app.route('/mail/adddomain/<name>', methods=['POST'])
 def mailadddomain(name):
-    user = 'mail.localdomain'
-    kwargs = {'name': name}
-    return jsonify(c.cmd(user, 'soy_router.route', ['dovecot', 'Mail', 'add_domain', kwargs]))
+    minion = 'mail.localdomain'
+    return jsonify(c.cmd(minion, 'soy_dovecot.add_domain', [name]))
 
 @app.route('/mail/adduser/<domainid>/<password>/<email>', methods=['POST'])
 def mailadduser(domainid, password, email):
-    user= 'mail.localdomain'
-    kwargs = {'domain_id': domainid,
-              'password': password,
-              'email': email}
-    return jsonify(c.cmd(user, 'soy_router.route', ['dovecot', 'Mail', 'add_user', kwargs]))
+    minion = 'mail.localdomain'
+    opts = {'domain_id': domainid,
+            'password': password,
+            'email': email}
+    return jsonify(c.cmd(minion, 'soy_dovecot.add_user', [**opts]))
 
 if __name__ == '__main__':
     server = WSGIServer(('',80),app)
