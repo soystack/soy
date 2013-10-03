a2833de58186483f8c445753ff1a64bdc2d29fca
James Albert <james.albert72@gmail.com>
2013-10-02 22:06:41 -0700

    removed useless db correction

--- salt/modules/soy_mysql.py
+++ salt/modules/soy_mysql.py
@@ -1,13 +1,10 @@
 def setup(user=False, db='mysql.'):
     if user:
         __pillar__ = __pillar__[user]
-	mysql = {
-		'host'	 : __pillar__['mysql.host'],
-		'port'	 : __pillar__['mysql.port'],
-		'user'	 : __pillar__['mysql.user'],
-		'passwd' : __pillar__['mysql.pass']
-	}
-    if db is not 'mysql.':
-        db = '%s.' % db
-    mysql['db'] = __pillar__[db+'db']
+	mysql = {'host'	 : __pillar__['mysql.host'],
+		 'port'	 : __pillar__['mysql.port'],
+		 'user'	 : __pillar__['mysql.user'],
+		 'passwd': __pillar__['mysql.pass'],
+		 'db':     __pillar__['mysql.db']}
+
 	return mysql
