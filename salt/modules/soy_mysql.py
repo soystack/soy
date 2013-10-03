39852b3787d7adf6ee0bebb12e4c72d7587bc883
James Albert <jbert@localhost.localdomain>
2013-10-02 21:40:47 -0700

    added soy_user_mysql.py, updated soy_mysql.py

--- salt/modules/soy_mysql.py
+++ salt/modules/soy_mysql.py
@@ -1,11 +1,13 @@
-#!/usr/bin/env python
-
-def setup(db):
+def setup(user=False, db='mysql.'):
+    if user:
+        __pillar__ = __pillar__[user]
 	mysql = {
-		'host'	 : __pillar__('mysql.host'),
-		'port'	 : __pillar__('mysql.port'),
-		'user'	 : __pillar__('mysql.user'),
-		'passwd' : __pillar__('mysql.pass'),
-		'db'	 : __pillar__[db]['db']
+		'host'	 : __pillar__['mysql.host'],
+		'port'	 : __pillar__['mysql.port'],
+		'user'	 : __pillar__['mysql.user'],
+		'passwd' : __pillar__['mysql.pass']
 	}
+    if db is not 'mysql.':
+        db = '%s.' % db
+    mysql['db'] = __pillar__[db+'db']
 	return mysql
---
+++ salt/modules/soy_user_mysql.py
@@ -0,0 +1,86 @@
+try:
+    from warnings import warn
+except:
+    warn = print
+try:
+    import MySQLdb as sql
+except ImportError:
+    warn('MySQL-python must be installed', ImportWarning)
+
+def connect(db=None):
+    try:
+        sqlconf = __salt__['soy_mysql.setup'](db)
+        conn = sql.connect(**sqlconf)
+        curs = sql.cursor()
+        return conn, curs
+    except sql.Error as e:
+        return False, 'mysql connection error: %s' % e.message
+    except:
+        warn('error while establishing connection', RuntimeWarning)
+        return False
+    finally:
+        if conn:
+            conn.close()
+
+def grant(**opts):
+    try:
+        conn, curs = connect()
+        permset = str()
+        for eachtype in perms:
+            permset += '%s,' % eachtype
+        opts['permset'] = permset[:-1]
+        curs.execute('''GRANT %(permset)s ON %(db)s.%(table)s TO "%(user)s"@"%(ip)s"''', **opts)
+        return True
+    except sql.Error as e:
+        return False, 'mysql error: %s' % e.message
+    except:
+        warn('error while granting permissions', RuntimeWarning)
+        return False
+    finally:
+        conn.close()
+
+def adduser(**opts):
+    try:
+        conn, curs = connect()
+        curs.execute('''CREATE USER "%(user)s"."%(ip)s" IDENTIFIED BY "%(passwd)s";
+                        GRANT SELECT, INSERT ON %(db)s.%(table)s TO "%(user)s"."%(ip)s";''', **opts)
+    except sql.Error as e:
+        return False, 'mysql error: %s' % e.message
+    except:
+        warn('error while creating user', RuntimeWarning)
+        return False
+    finally:
+        conn.close()
+
+def createdb(**opts):
+    try:
+        conn, curs = connect()
+        curs.execute('''CREATE DATABASE IF NOT EXISTS %(db)s;
+                        GRANT ALL ON %(db)s.* TO "%(user)s"."%(ip)s";''', **opts)
+        return True
+    except sql.Error as e:
+        return False, 'mysql error: %s' % e.message
+    except:
+        warn('error while creating db', RuntimeWarning)
+    finally:
+        conn.close()
+
+def createtable(db, name, **fields):
+    try:
+        conn, curs = connect(db)
+        query = ''
+        for k, v in fields.iteritems():
+            '''
+            example k, v:
+                k => 'id'
+                v => 'INT AUTO_INCREMENT'
+            '''
+            query += '%s %s,' % (k, v)
+        curs.execute('''CREATE TABLE %s (%s)''', (name, query[:-1],))
+        return True
+    except sql.Error as e:
+        return False, 'mysql error: %s' % e.message
+    except:
+        warn('error whilst creating table', RuntimeWarning)
+    finally:
+        conn.close()
