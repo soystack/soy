7c85b7feb41d612be0d5905d21c565f36cc8f1ad
jamesalbert <jbert@localhost.localdomain>
2013-10-03 12:06:17 -0700

    added additional sql functions

--- salt/modules/soy_user_mysql.py
+++ salt/modules/soy_user_mysql.py
@@ -15,7 +15,8 @@ def connect(db=None):
         curs = sql.cursor()
         return conn, curs
     except sql.Error as e:
-        return False, 'mysql connection error: %s' % e.message
+        warn('mysql error: %s' % e.message, RuntimeWarning)
+        return False
     except:
         warn('error while establishing connection', RuntimeWarning)
         return False
@@ -33,7 +34,8 @@ def grant(**opts):
         curs.execute('''GRANT %(perms)s ON %(db)s.%(table)s TO "%(user)s"@"%(ip)s"''', **opts)
         return True
     except sql.Error as e:
-        return False, 'mysql error: %s' % e.message
+        warn('mysql error: %s' % e.message, RuntimeWarning)
+        return False
     except:
         warn('error while granting permissions', RuntimeWarning)
         return False
@@ -47,8 +49,10 @@ def adduser(**opts):
                         GRANT SELECT, INSERT ON %(db)s.%(table)s TO "%(user)s"@"%(ip)s";
                         GRANT USAGE ON %(db)s.%(table)s
                         TO "%(user)s"@"%(ip)s" WITH MAX_QUERIES_PER_HOUR 100''', **opts)
+        return True
     except sql.Error as e:
-        return False, 'mysql error: %s' % e.message
+        warn('mysql error: %s' % e.message, RuntimeWarning)
+        return False
     except:
         warn('error while creating user', RuntimeWarning)
         return False
@@ -59,8 +63,56 @@ def deleteuser(**opts):
     try:
         conn, curs = connect(opts['db'])
         curs.execute('''DROP USER %(user)s@%(ip)s''', **opts)
+        return True
     except sql.Error as e:
-        return False, 'mysql error: %s' % e.message
+        warn('mysql error: %s' % e.message, RuntimeWarning)
+        return False
+    except:
+        warn('error while deleting user', RuntimeWarning)
+        return False
+    finally:
+        conn.close()
+
+def renameuser(**opts):
+    try:
+        conn, curs = connect(opts['db'])
+        curs.execute('''RENAME USER %(user)s TO %(newuser)s''', **opts)
+        return True
+    except sql.Error as e:
+        warn('mysql error: %s' % e.message, RuntimeWarning)
+        return False
+    except:
+        warn('error while deleting user', RuntimeWarning)
+        return False
+    finally:
+        conn.close()
+
+def revokeuser(**opts):
+    try:
+        conn, curs = connect(opts['db'])
+        permset = str()
+        for eachtype in opts['perms']:
+            permset += '%s,' % eachtype
+        opts['perms'] = permset[:-1]
+        curs.execute('''REVOKE %(perms)s ON %(db)s.%(table) FROM "%(user)s"@"%(ip)s"''', **opts)
+        return True
+    except sql.Error as e:
+        warn('mysql error: %s' % e.message, RuntimeWarning)
+        return False
+    except:
+        warn('error while deleting user', RuntimeWarning)
+        return False
+    finally:
+        conn.close()
+
+def setpasswduser(**opts):
+    try:
+        conn, curs = connect(opts['db'])
+        curs.execute('''SET PASSWORD FOR "%(user)s"@"%(ip)s" = PASSWORD("%(newpasswd)s")''', **opts)
+        return True
+    except sql.Error as e:
+        warn('mysql error: %s' % e.message, RuntimeWarning)
+        return False
     except:
         warn('error while deleting user', RuntimeWarning)
         return False
@@ -71,8 +123,10 @@ def update(**opts):
     try:
         conn, curs = connect(opts['db'])
         curs.execute('''UPDATE %(db)s.%(table) SET %(key)s = %(value)s WHERE %(cond)s = %(exp)s''', **opts)
+        return True
     except sql.Error as e:
-        return False, 'mysql error: %s' % e.message
+        warn('mysql error: %s' % e.message, RuntimeWarning)
+        return False
     except:
         warn('error while updating field', RuntimeWarning)
         return False
@@ -83,8 +137,10 @@ def report(**opts):
     try:
         conn, curs = connect(opts['db'])
         curs.execute('''SELECT %(column)s FROM %(db)s.%(table)s WHERE $(cond)s = %(exp)s''', **opts)
+        return curs.fetchall()
     except sql.Error as e:
-        return False, 'mysql error: %s' % e.message 
+        warn('mysql error: %s' % e.message, RuntimeWarning)
+        return False
     except:
         warn('error while reporting rows', RuntimeWarning)
         return False
@@ -95,8 +151,10 @@ def deleterow(**opts):
     try:
         conn, curs = connect(opts['db'])
         curs.execute('''DROP FROM %(db)s.%(table)s WHERE %(cond)s = %(exp)s''', **opts)
+        return True
     except sql.Error as e:
-        return False, 'mysql error: %s' % e.message
+        warn('mysql error: %s' % e.message, RuntimeWarning)
+        return False
     except:
         warn('error while deleting row', RuntimeWarning)
         return False
@@ -107,8 +165,10 @@ def dropdb(**opts):
     try:
         conn, curs = connect()
         curs.execute('''DROP DATABASE %(db)s''', **opts)
+        return True
     except sql.Error as e:
-        return False, 'mysql error: %s' % e.message
+        warn('mysql error: %s' % e.message, RuntimeWarning)
+        return False
     except:
         warn('error while dropping db. most likely syntax related', UserWarning)
         return False
@@ -122,9 +182,110 @@ def createdb(**opts):
                         GRANT ALL ON %(db)s.* TO "%(user)s"@"%(ip)s";''', **opts)
         return True
     except sql.Error as e:
-        return False, 'mysql error: %s' % e.message
+        warn('mysql error: %s' % e.message, RuntimeWarning)
+        return False
     except:
         warn('error while creating db', RuntimeWarning)
+        return False
+    finally:
+        conn.close()
+
+def repairtable(**opts):
+    try:
+        conn, curs = connect(opts['db'])
+        curs.execute('''REPAIR TABLE %(db)s.%(table)s''', **opts)
+        return True
+    except sql.Error as e:
+        warn('mysql error: %s' % e.message, RuntimeWarning)
+        return False
+    except:
+        warn('error while repairing table', RuntimeWarning)
+        return False
+    finally:
+        conn.close()
+
+def backuptable(**opts):
+    try:
+        conn, curs = connect(opts['db'])
+        tableset = str()
+        for table in opts['table']:
+            tableset += '%s,' % table
+        opts['table'] = tableset[:-1]
+        curs.execute('''BACKUP TABLE %(db)s.%(table) TO %(dir)s''', **opts)
+        return True
+    except sql.Error as e:
+        warn('mysql error: %s' % e.message, RuntimeWarning)
+        return False
+    except:
+        warn('error while backing up table', RuntimeWarning)
+        return False
+    finally:
+        conn.close()
+
+def restoretable(**opts):
+    try:
+        conn, curs = connect(opts['db'])
+        tableset = str()
+        for table in opts['table']:
+            tableset += '%s,' % table
+        opts['table'] = tableset[:-1]
+        curs.execute('''RESTORE TABLE %(db)s.%(table)s FROM %(dir)s''', **opts)
+        return True
+    except sql.Error as e:
+        warn('mysql error: %s' % e.message, RuntimeWarning)
+        return False
+    except:
+        warn('error while restoring table', RuntimeWarning)
+        return False
+    finally:
+        conn.close()
+
+def analyzetable(**opts):
+    try:
+        conn, curs = connect(opts['db'])
+        for table in opts['table']:
+            tableset += '%s,' % table
+        opts['table'] = tableset[:-1]
+        curs.execute('''ANALYZE TABLE %(db)s.%(table)s''', **opts)
+        return curs.fetchall()
+    except sql.Error as e:
+        warn('mysql error: %s' % e.message, RuntimeWarning)
+        return False
+    except:
+        warn('error while analyzing table', RuntimeWarning)
+        return False
+    finally:
+        conn.close()
+
+def optimizetable(**opts):
+    try:
+        conn, curs = connect(opts['db'])
+        for table in opts['table']:
+            tableset += '%s,' % table
+        opts['table'] = tableset[:-1]
+        curs.execute('''OPTIMIZE TABLE %(db)s.%(table)s''', **opts)
+    except sql.Error as e:
+        warn('mysql error: %s' % e.message, RuntimeWarning)
+        return False
+    except:
+        warn('error while analyzing table', RuntimeWarning)
+        return False
+    finally:
+        conn.close()
+
+def checksumtable(**opts):
+    try:
+        conn, curs - connect(opts['db'])
+        for table in opts['table']:
+            tableset += '%s,' % table
+        opts['table'] = tableset[:-1]
+        curs.execute('''CHECKSUM TABLE %(table)s %(method)s''', **opts)
+        return curs.fetchall()
+    except sql.Error as e:
+        warn('mysql error: %s' % e.message, RuntimeWarning)
+        return False
+    except:
+        warn('error while analyzing table', RuntimeWarning)
         return False
     finally:
         conn.close()
@@ -144,7 +305,8 @@ def createtable(**opts):
         curs.execute('''CREATE TABLE %(db)s.%(table)s (%(fields)s)''', **opts)
         return True
     except sql.Error as e:
-        return False, 'mysql error: %s' % e.message
+        warn('mysql error: %s' % e.message, RuntimeWarning)
+        return False
     except:
         warn('error whilst creating table', RuntimeWarning)
         return False
